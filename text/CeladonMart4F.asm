38fb8741d998d5c19259d9635c0b10fb8a552450
poBBpC <125530679+poBBpC@users.noreply.github.com>
2024-09-29 19:43:19 -0400

    Update CeladonMart4F.asm

--- text/CeladonMart4F.asm
+++ text/CeladonMart4F.asm
@@ -1,6 +1,6 @@
 _CeladonMart4Text2::
 	text "I'm getting a"
-	line "PIPPI DOLL for my"
+	line "PIPPY DOLL for my"
 	cont "girl friend!"
 	done
 
@@ -11,7 +11,7 @@ _CeladonMart4Text3::
 	para "You can run from"
 	line "wild #MON by"
 	cont "distracting them"
-	cont "with a PIPPI DOLL!"
+	cont "with a PIPPY DOLL!"
 	done
 
 _CeladonMart4Text4::
