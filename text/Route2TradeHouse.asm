1ad7d9fc123f94e7015b26611fbb9f033755e4dd
poBBpC <125530679+poBBpC@users.noreply.github.com>
2024-09-29 21:24:37 -0400

    Update Route2TradeHouse.asm

--- text/Route2TradeHouse.asm
+++ text/Route2TradeHouse.asm
@@ -1,6 +1,8 @@
 _Route2HouseText1::
 	text "A fainted #MON"
-	line "can't fight. But, "
-	cont "it can still use "
-	cont "moves like CUT!"
+	line "can't fight. But,"
+	cont "it can still use"
+	cont "moves it knows"
+	cont "outside battles"
+	cont "like IAI CUT!"
 	done
